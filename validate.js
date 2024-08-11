984ddf0a745a721be6e3b2aaafb8751b0ef67f23
erfnjhdi <136998922+erfnjhdi@users.noreply.github.com>
2024-08-10 20:28:55 -0400

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
